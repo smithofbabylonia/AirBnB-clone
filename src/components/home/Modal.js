820c3d3f6e2c3e57ecb2e3ee24ed8d5025ac7c97
Martin Malaza <martinmalaza59@gmail.com>
2023-04-17 19:57:53 +0200

    Modal can accept any component

--- src/components/home/Modal.js
+++ src/components/home/Modal.js
@@ -1,14 +1,33 @@
+import { useDispatch, useSelector } from "react-redux";
+import { openModal } from "../../action/modalActions";
 
 /*
 This component is gonna be used by multiple other components
 therefore we have to figure out how to trigger it in all of them
 */
 
+
 function Modal(props){
 
+    const dispatch = useDispatch();
+    const modal = useSelector(state => state.modal);
+    const {openClose, content} = modal;
+    let modalStyle;
+
+    function closeModalHandle(event){
+        //console.log("pressed the",event.target.id);
+        if(event.target.id==="modal") dispatch(openModal("close",""));
+    }
+
+    if(openClose==="open"){
+        modalStyle = {display: "block"};
+    }else{
+        modalStyle = {display: "none"};
+    }
+
     return(
-        <div className="modal">
-            {props.children}
+        <div className="modal" onClick={closeModalHandle} style={modalStyle} id="modal">
+            {content}
         </div>
     );
 }
